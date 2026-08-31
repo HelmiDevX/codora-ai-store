b6c53c25a996ec69815705db227122fcaf4e774b
helmi533 <helmiiamihassan71610@gmail.com>
2026-09-01 00:09:44 +0300

    Remove route segment config from client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 import React, { useState, useEffect } from 'react';
 import { ProductCategory, Product } from '@/types/product';
 import { CategoryFilter } from '@/components/storefront/category-filter';
